dfe7f322ff285bfb00e731c7d747ec39989bfaf6
PeppaYao <m13166431327@163.com>
2020-10-12 01:01:45 +0800

    添加P命名和C命名

--- spring-04-di/src/test/java/MyTest.java
+++ spring-04-di/src/test/java/MyTest.java
@@ -1,4 +1,6 @@
 import com.bytedance.pojo.Student;
+import com.bytedance.pojo.User;
+import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -7,5 +9,13 @@ public class MyTest {
        ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
         Student student1 = (Student) context.getBean("student_1");
         System.out.println(student1.toString());
+    }
+
+    @Test
+    public void test2(){
+        ApplicationContext context = new ClassPathXmlApplicationContext("bean.xml");
+        User user = context.getBean("user2", User.class);
+        System.out.println(user.toString());
+
     }
 }
